8c2dcb6d7885f934707e79fc34e98d8cc1a3e333
Mike <ignorant.instigator@gmail.com>
2014-05-10 15:26:00 +0300

    Minor changes

--- TicTacToe/Client/MainFrame.java
+++ TicTacToe/Client/MainFrame.java
@@ -1,8 +1,32 @@
+import java.awt.*;
+import javax.swing.*;
 
 public class MainFrame {
+	LogIn log ;
+	JFrame frame;
+
+	MainFrame() {
+		setFrame();
+		log = new LogIn(frame);
+		log.adjustAppearance();
+	}
+
+	void setFrame() {
+		frame = new JFrame("Client");
+		frame.setLayout(new BorderLayout());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setSize(300, 300);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new MainFrame();
+			}
+		});
 
 	}
 
